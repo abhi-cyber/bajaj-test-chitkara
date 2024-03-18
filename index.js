3b493bd5880421a7dd3941adbee36859e0ee90b6
Abhiraj <abhirajchatrath@gmail.com>
2024-03-18 11:36:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = "https://bajaj-test-abhiraj-2110990057.netlify.app/";
 
 app.use(express.json());
 
